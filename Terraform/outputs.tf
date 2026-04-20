687e2a5bb266b70dda550c6592534aeba0439929
Sachin Singh <150858710+Sachin8801@users.noreply.github.com>
2026-04-20 12:59:45 -0500

    Updated outputs for connection commands

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -5,3 +5,15 @@ output "cluster_name" {
 output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
+
+
+# Quick commands
+output "configure_kubectl" {
+  description = "Command to configure kubectl"
+  value       = "aws eks update-kubeconfig --name ${module.eks.cluster_name} --region ${local.region}"
+}
+
+output "argocd_initial_password" {
+  description = "Command to get ArgoCD initial admin password"
+  value       = "kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath='{.data.password}' | base64 -d"
+}
